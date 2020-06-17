93fdbc6d797bbcf53df484a8446831b628a644e2
bronco-one <zdzislaw.bronikowski@gmail.com>
2020-06-17 19:27:07 +0200

    12

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=menu',
+    'username' => 'bronco',
+    'password' => 'pingwin67A',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
