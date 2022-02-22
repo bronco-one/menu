51a2e08d3b11457503bbe84be46c5525eb2efa09
Zdzisław Bronikowski <zdzislaw.bronikowski@gmail.com>
2022-02-22 19:05:40 +0100

    no message

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,23 +1,23 @@
  <?php
-/* @var $this yii\web\View */
+    /* @var $this yii\web\View */
 
-use yii\helpers\Url;
-use yii\helpers\Html;
-use Yii;
+    use yii\helpers\Url;
+    use yii\helpers\Html;
+    use Yii;
 
-$this->title = 'Baszta';
-?>
+    $this->title = 'Baszta';
+    ?>
  <section class="page-section clearfix">
      <div class="container">
          <div class="intro">
              <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo Url::to('@web/img/intro.jpg') ?>" alt="..." />
              <div class="intro-text left-0 text-center bg-faded p-5 rounded">
                  <h2 class="section-heading mb-4">
-                     <span class="section-heading-upper">Fresh Coffee</span>
-                     <span class="section-heading-lower">Worth Drinking</span>
+                     <span class="section-heading-upper">Zapraszamy na wyśmienitą Kawę!</span>
+                     <span class="section-heading-lower">Warto dobrze rozpocząć dzień</span>
                  </h2>
                  <p class="mb-3">Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!</p>
-                 <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="#!">Visit Us Today!</a></div>
+                 <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="#!">Odwiedź nas!</a></div>
              </div>
          </div>
      </div>
@@ -28,7 +28,7 @@ $this->title = 'Baszta';
              <div class="col-xl-9 mx-auto">
                  <div class="cta-inner bg-faded text-center rounded">
                      <h2 class="section-heading mb-4">
-                         <span class="section-heading-upper">Our Promise</span>
+                         <span class="section-heading-upper">Nasza Obietnica</span>
                          <span class="section-heading-lower">To You</span>
                      </h2>
                      <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
