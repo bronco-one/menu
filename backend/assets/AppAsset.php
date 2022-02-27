b3d685eb3c7179ff30cc651161d2427d8fc42b54
Zdzisław Bronikowski <zdzislaw.bronikowski@gmail.com>
2022-02-27 10:46:10 +0100

    no message

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,8 +13,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/sb-admin-2.min.css'
     ];
     public $js = [
+        'js/sb-admin-2.min.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
